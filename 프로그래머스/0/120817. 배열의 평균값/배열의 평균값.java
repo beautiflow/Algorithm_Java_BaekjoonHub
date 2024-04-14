30d7b8e585bc003b0ba173c21bc2ea033f922dbf
NaYeon Oh <105831341+beautiflow@users.noreply.github.com>
2024-04-15 00:42:49 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,16 +1,6 @@
 class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
-        double sum = 0;
-        double multi = 0;
-        
-        
-        for(int i = 0; i < numbers.length; i++){
-             multi = numbers[i];
-             sum += multi;
-        }
-        answer = sum/numbers.length;
-
         return answer;
     }
 }
