258cb4f9d705c02d51aa00e09eebc357d1e69a8e
NaYeon Oh <105831341+beautiflow@users.noreply.github.com>
2024-05-21 21:46:48 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        int service = n/10;
+        
+        k -= service;
+        
+        answer = (n*12000) + (k*2000);
+        
+        
+        return answer;
+    }
+}
