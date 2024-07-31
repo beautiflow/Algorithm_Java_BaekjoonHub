bbd6c8fd89403867170195e16da36858a5b618c3
NaYeon Oh <105831341+beautiflow@users.noreply.github.com>
2024-08-01 08:50:51 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        int num = message.length();
+        
+        answer = num * 2;
+        
+        return answer;
+    }
+}
