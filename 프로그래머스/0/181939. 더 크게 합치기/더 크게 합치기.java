104c3c95ed44cb37ed0609e448e213f3ba62f0bb
NaYeon Oh <105831341+beautiflow@users.noreply.github.com>
2024-09-03 22:53:29 +0900

    [level 0] Title: 더 크게 합치기, Time: 2.21 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        String A = String.valueOf(a);
+        String B = String.valueOf(b);
+        
+        String AB = A+B;
+        String BA = B+A;
+        
+        int ab = Integer.parseInt(AB);
+        int ba = Integer.parseInt(BA);
+        
+        if(ab > ba){
+            answer = ab;
+        }
+        else if(ab < ba){
+            answer = ba;
+        }
+        else{
+            answer = ab;
+        }
+        
+        return answer;
+    }
+}
